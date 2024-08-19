79d490830ccbc9867cb88c1bde5b96750e7b5e18
Siddharth Bansal <sidbansal@Siddharths-MacBook-Air.local>
2024-08-18 22:29:49 -0700

    Support for Piazza database

--- image/src/main.py
+++ image/src/main.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 
-ARXIV_CLASSIFICATION_URL = "http://35.247.118.232:8000"
+COMPUTE_ENGINE_URL = "http://34.82.168.208:8000"
 PING_REQUEST = {
     "statusCode": 200,
     "headers": {
@@ -22,7 +22,8 @@ def make_good_response(data):
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin' : "https://sidhub.net",
+            'Access-Control-Allow-Origin' : "*",
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
             'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
             'Access-Control-Allow-Credentials' : True,
         },
@@ -31,9 +32,16 @@ def make_good_response(data):
 
 def handle_arxiv_classification(data):
 
-    response = requests.post(ARXIV_CLASSIFICATION_URL, json={'data': data, 'resource': 'arxivClassification'})
+    response = requests.post(COMPUTE_ENGINE_URL, 
+                            json={'data': data, 'resource': 'arxivClassification'}
+                            )
     return response.json()
 
+def handle_360_piazza_database(data):
+    response = requests.post(COMPUTE_ENGINE_URL, 
+                            json={'data': data, 'resource': '360PiazzaDatabase'}
+                            )
+    return response.json()
 
 def handler(event, context):
 
@@ -43,13 +51,20 @@ def handler(event, context):
     path = event['path']
 
     if path == '/Projects/arxivClassification' and event['body'] and 'data' in event['body']:
-        
+                
         query = json.loads(event['body'])['data']
         classification = handle_arxiv_classification(query)
         
         return make_good_response(
         {
             'classification': classification['message'],
+        })
+
+    if path == '/Projects/360PiazzaDatabase' and event['body'] and 'data' in event['body']:
+        query = json.loads(event['body'])['data']
+        json_response = handle_360_piazza_database(query)
+        return make_good_response({
+            'database_response': json_response
         })
         
     return PING_REQUEST
